03589b96407bb361c1353f6f1d2c72b9453549da
Drew Nauman <din3@cornell.edu>
2016-07-12 13:15:57 -0600

    add documentation

--- client/app/app.js
+++ client/app/app.js
@@ -1,17 +1,22 @@
 var app = angular.module("usedCarApp", ["ui.router", "ngStorage", "ngResource", "ui.bootstrap"]);
 
 app.config(["$stateProvider", "$urlRouterProvider", "$httpProvider", "$resourceProvider", function ($stateProvider, $urlRouterProvider, $httpProvider, $resourceProvider) {
+    //runs AuthInterceptor on all http requests
     $httpProvider.interceptors.push("AuthInterceptor");
-//    $urlRouterProvider.otherwise('/home');
+    // Undefined routes redirect to home
+    $urlRouterProvider.otherwise('/home');
     // Don't strip trailing slashes from calculated URLs
     $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
 
+//Controller for the navbar
 app.controller("mainCtrl", ["$scope", "UserService", function ($scope, UserService) {
     console.log("main controller");
+    //gives navbar html access to UserService
     $scope.userService = UserService;
 }])
 
+//Intercept HTTP requests and add the User Auth token to the header
 app.factory("AuthInterceptor", ["$location", "$q", "TokenService", function ($location, $q, TokenService) {
     return {
         request: function (config) {
